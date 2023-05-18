b4e617d72d113c5c3fbd384d6d148e326d67a1d8
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-05-18 14:10:52 +0100

    ESWE-600_prepare_for_development Added shared code, prep for dev (#4)
    * ESWE-600_prepare_for_development Added shared code, prep for dev
    
    * ESWE-600_prepare_for_development Fixed integration tests

---
+++ server/middleware/setUpLocals.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import contentLookup from '../constants/contentLookup'
+import addressLookup from '../routes/addressLookup'
+
+// Add constants and utilities to locals
+export default function setUpLocals(): Router {
+  const router = Router({ mergeParams: true })
+
+  router.use((req, res, next) => {
+    res.locals.addressLookup = addressLookup
+    res.locals.contentLookup = contentLookup
+    res.locals.originalUrl = req.originalUrl
+
+    next()
+  })
+
+  return router
+}
--- server/services/index.ts
+++ server/services/index.ts
@@ -1,13 +1,37 @@
 import { dataAccess } from '../data'
 import UserService from './userService'
+import PrisonerSearchService from './prisonSearchService'
+import PaginationService from './paginationServices'
+import CuriousEsweService from './curiousEsweService'
+import KeyworkerService from './keyworkerService'
+import PrisonService from './prisonService'
+import WhereaboutsService from './whereaboutsService'
+import AllocationManagerService from './allocationManagerService'
+import CommunityService from './communityService'
 
 export const services = () => {
   const { hmppsAuthClient } = dataAccess()
 
   const userService = new UserService(hmppsAuthClient)
+  const prisonerSearchService = new PrisonerSearchService(hmppsAuthClient)
+  const paginationService = new PaginationService()
+  const curiousEsweService = new CuriousEsweService(hmppsAuthClient)
+  const keyworkerService = new KeyworkerService(hmppsAuthClient)
+  const prisonService = new PrisonService(hmppsAuthClient)
+  const whereaboutsService = new WhereaboutsService(hmppsAuthClient)
+  const allocationManagerService = new AllocationManagerService(hmppsAuthClient)
+  const communityService = new CommunityService(hmppsAuthClient)
 
   return {
     userService,
+    prisonerSearchService,
+    paginationService,
+    curiousEsweService,
+    keyworkerService,
+    prisonService,
+    whereaboutsService,
+    allocationManagerService,
+    communityService,
   }
 }
 
