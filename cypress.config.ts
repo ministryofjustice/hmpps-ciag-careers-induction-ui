73caf580e8547abbf37cfcfba603ceed6f2ff72b
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-05-22 11:32:19 +0100

    ESWE-589_profile_page_header Added plan page (#6)
    * ESWE-589_profile_page_header Added plan page
    
    * ESWE-589_profile_page_header Added missing test

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { resetStubs } from './integration_tests/mockApis/wiremock'
 import auth from './integration_tests/mockApis/auth'
 import tokenVerification from './integration_tests/mockApis/tokenVerification'
 import nomisUserRolesApi from './integration_tests/mockApis/nomisUserRolesApi'
+import prisonerSearchApi from './integration_tests/mockApis/prisonerSearchApi'
 
 export default defineConfig({
   chromeWebSecurity: false,
@@ -24,6 +25,7 @@ export default defineConfig({
         ...auth,
         ...tokenVerification,
         ...nomisUserRolesApi,
+        ...prisonerSearchApi,
       })
     },
     baseUrl: 'http://localhost:3007',
