78c1c75f1cd849fa1af007b0863ce6e0b5c35e46
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-05-23 13:50:01 +0100

    Eswe 592 hoping to get work page (#9)
    * ESWE-592_hoping_to_get_work_page Added hope to get work page
    
    * ESWE-592_hoping_to_get_work_page Added integration tests
    
    * ESWE-592_hoping_to_get_work_page Updated config

---
+++ integration_tests/e2e/hopingToGetWork.cy.ts
@@ -0,0 +1,64 @@
+import HopingToGetWorkPage from '../pages/hopingToGetWork'
+
+context('SignIn', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+    cy.task('getPrisonerById')
+    cy.task('getUserActiveCaseLoad')
+    cy.task('stubVerifyToken', true)
+  })
+
+  it('Validation messages display when no value selected', () => {
+    cy.signIn()
+
+    cy.visit('/plan/create/G6115VJ/hoping-to-get-work')
+
+    const hopingToGetWorkPage = new HopingToGetWorkPage("Is Daniel Craig hoping to get work when they're released?")
+
+    hopingToGetWorkPage.submitButton().click()
+
+    hopingToGetWorkPage.pageErrorMessage().contains('Select whether Daniel Craig is hoping to get work')
+    hopingToGetWorkPage.fieldErrorMessage().contains('Select whether Daniel Craig is hoping to get work')
+  })
+
+  it('New record - Select YES - navigates to qualifications page', () => {
+    cy.signIn()
+
+    cy.visit('/plan/create/G6115VJ/hoping-to-get-work')
+
+    const hopingToGetWorkPage = new HopingToGetWorkPage("Is Daniel Craig hoping to get work when they're released?")
+
+    hopingToGetWorkPage.radioFieldValue('YES').click()
+    hopingToGetWorkPage.submitButton().click()
+
+    cy.url().should('include', 'qualifications/new')
+  })
+
+  it('New record - Select NO - navigates to why-no-work page', () => {
+    cy.signIn()
+
+    cy.visit('/plan/create/G6115VJ/hoping-to-get-work')
+
+    const hopingToGetWorkPage = new HopingToGetWorkPage("Is Daniel Craig hoping to get work when they're released?")
+
+    hopingToGetWorkPage.radioFieldValue('NO').click()
+    hopingToGetWorkPage.submitButton().click()
+
+    cy.url().should('include', 'why-no-work/new')
+  })
+
+  it('New record - Select NO_SURE - navigates to why-no-work page', () => {
+    cy.signIn()
+
+    cy.visit('/plan/create/G6115VJ/hoping-to-get-work')
+
+    const hopingToGetWorkPage = new HopingToGetWorkPage("Is Daniel Craig hoping to get work when they're released?")
+
+    hopingToGetWorkPage.radioFieldValue('NOT_SURE').click()
+    hopingToGetWorkPage.submitButton().click()
+
+    cy.url().should('include', 'why-no-work/new')
+  })
+})
