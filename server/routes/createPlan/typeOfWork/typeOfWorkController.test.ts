ca03bb2f9b8dcf44a80e41271478467e9f7326f4
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-06-20 16:10:35 +0100

    Eswe 624 type of work page (#18)
    * ESWE-624_type_of_work_page added page scaffolding
    
    * ESWE-624_type_of_work_page Completed page logic and content
    
    * ESWE-624_type_of_work_page Added integration tests
    
    * ESWE-624_type_of_work_page Fixed type issue

---
+++ server/routes/createPlan/typeOfWork/typeOfWorkController.test.ts
@@ -0,0 +1,157 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { plainToClass } from 'class-transformer'
+
+import expressMocks from '../../../testutils/expressMocks'
+import Controller from './typeOfWorkController'
+import validateFormSchema from '../../../utils/validateFormSchema'
+import addressLookup from '../../addressLookup'
+import TypeOfWorkValue from '../../../enums/typeOfWorkValue'
+import { getSessionData, setSessionData } from '../../../utils/session'
+import PrisonerViewModel from '../../../viewModels/prisonerViewModel'
+import pageTitleLookup from '../../../utils/pageTitleLookup'
+import HopingToGetWorkValue from '../../../enums/hopingToGetWorkValue'
+
+jest.mock('../../../utils/pageTitleLookup', () => ({
+  ...jest.requireActual('../../../utils/pageTitleLookup'),
+  __esModule: true,
+  default: jest.fn(),
+}))
+
+jest.mock('../../../utils/validateFormSchema', () => ({
+  ...jest.requireActual('../../../utils/validateFormSchema'),
+  __esModule: true,
+  default: jest.fn(),
+}))
+
+jest.mock('./validationSchema', () => ({
+  ...jest.requireActual('./validationSchema'),
+  __esModule: true,
+  default: jest.fn(),
+}))
+
+describe('TypeOfWorkController', () => {
+  const { req, res, next } = expressMocks()
+
+  const pageTitleLookupMock = pageTitleLookup as jest.Mock
+  pageTitleLookupMock.mockReturnValue('mock_page_title')
+
+  req.context.prisoner = {
+    firstName: 'mock_firstName',
+    lastName: 'mock_lastName',
+  }
+
+  req.params.id = 'mock_ref'
+  req.params.mode = 'new'
+  const { id, mode } = req.params
+
+  const mockData = {
+    backLocation: addressLookup.createPlan.hasWorkedBefore(id, mode),
+    backLocationAriaText: 'Back to mock_page_title',
+    prisoner: plainToClass(PrisonerViewModel, req.context.prisoner),
+    typeOfWork: [] as any,
+  }
+
+  res.locals.user = {}
+
+  const controller = new Controller()
+
+  describe('#get(req, res)', () => {
+    beforeEach(() => {
+      res.render.mockReset()
+      next.mockReset()
+      setSessionData(req, ['typeOfWork', id, 'data'], mockData)
+      setSessionData(req, ['createPlan', id], {
+        hopingToGetWork: HopingToGetWorkValue.YES,
+      })
+    })
+
+    it('On error - Calls next with error', async () => {
+      res.render.mockImplementation(() => {
+        throw new Error('mock_error')
+      })
+      controller.get(req, res, next)
+
+      expect(next).toHaveBeenCalledTimes(1)
+    })
+
+    it('On success - No record found - Calls render with the correct data', async () => {
+      controller.get(req, res, next)
+
+      expect(res.render).toHaveBeenCalledWith('pages/createPlan/typeOfWork/index', { ...mockData })
+      expect(next).toHaveBeenCalledTimes(0)
+    })
+
+    it('On success - Record found - Calls render with the correct data', async () => {
+      setSessionData(req, ['createPlan', id], {
+        hopingToGetWork: HopingToGetWorkValue.YES,
+        typeOfWork: TypeOfWorkValue.OTHER,
+      })
+      req.params.mode = 'edit'
+
+      controller.get(req, res, next)
+
+      expect(res.render).toHaveBeenCalledWith('pages/createPlan/typeOfWork/index', {
+        ...mockData,
+        backLocation: addressLookup.createPlan.checkAnswers(id),
+        typeOfWork: TypeOfWorkValue.OTHER,
+      })
+      expect(next).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  describe('#post(req, res)', () => {
+    const errors = { details: 'mock_error' }
+
+    const validationMock = validateFormSchema as jest.Mock
+
+    beforeEach(() => {
+      res.render.mockReset()
+      res.redirect.mockReset()
+      next.mockReset()
+      validationMock.mockReset()
+      setSessionData(req, ['typeOfWork', id, 'data'], mockData)
+      setSessionData(req, ['createPlan', id], {
+        hopingToGetWork: HopingToGetWorkValue.YES,
+      })
+    })
+
+    it('On error - Calls next with error', async () => {
+      validationMock.mockImplementation(() => {
+        throw new Error('mock_error')
+      })
+
+      controller.post(req, res, next)
+
+      expect(next).toHaveBeenCalledTimes(1)
+      expect(res.render).toHaveBeenCalledTimes(0)
+    })
+
+    it('On validation error - Calls render with the correct data', async () => {
+      validationMock.mockImplementation(() => errors)
+
+      controller.post(req, res, next)
+
+      expect(res.render).toHaveBeenCalledWith('pages/createPlan/typeOfWork/index', {
+        ...mockData,
+        errors,
+      })
+      expect(next).toHaveBeenCalledTimes(0)
+    })
+
+    it('On success - mode = new - Sets session record then redirects to workDetails', async () => {
+      req.body.typeOfWork = [TypeOfWorkValue.OTHER]
+      req.body.typeOfWorkDetails = 'mock_details'
+      req.params.mode = 'new'
+
+      controller.post(req, res, next)
+
+      expect(getSessionData(req, ['createPlan', id])).toEqual({
+        hopingToGetWork: 'YES',
+        typeOfWork: [TypeOfWorkValue.OTHER],
+        typeOfWorkDetails: 'mock_details',
+      })
+      expect(getSessionData(req, ['typeOfWork', id, 'data'])).toBeFalsy()
+      expect(res.redirect).toHaveBeenCalledWith(addressLookup.createPlan.workDetails(id, TypeOfWorkValue.OTHER))
+    })
+  })
+})
