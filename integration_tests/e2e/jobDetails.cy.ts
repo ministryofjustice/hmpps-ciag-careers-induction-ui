34f61d5ce7ab1d3bfc85a9d1e6f32a444df857fa
mons kapinga <mons.kapinga@digital.justice.gov.uk>
2023-06-28 10:08:42 +0100

    ESWE-596: fix jobDetails cypress testing

--- integration_tests/e2e/jobDetails.cy.ts
+++ integration_tests/e2e/jobDetails.cy.ts
@@ -15,6 +15,7 @@ context('Job details page', () => {
     cy.task('getUserActiveCaseLoad')
     cy.task('stubVerifyToken', true)
     cy.task('getLearnerEducation')
+    cy.task('stubCiagListByCaseloadId', 'MDI')
     cy.signIn()
 
     cy.visit('/plan/create/G6115VJ/hoping-to-get-work')
