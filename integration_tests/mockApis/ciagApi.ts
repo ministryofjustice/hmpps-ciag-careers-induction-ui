c5e423f22e042f0493e779550d5d5beb059dcf21
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-08-22 11:50:24 +0100

    ESWE-635_check_answers_page Fixed failing test

--- integration_tests/mockApis/ciagApi.ts
+++ integration_tests/mockApis/ciagApi.ts
@@ -1,19 +1,8 @@
 import { stubFor } from './wiremock'
 
-const getCiagPlan = (id = 'G6115VJ') =>
-  stubFor({
-    request: {
-      method: 'GET',
-      urlPattern: `/ciag/${id}`,
-    },
-    response: {
-      status: 200,
-      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-      jsonBody: {
-        offenderId: id,
-      },
-    },
-  })
+import plans from '../mockData/ciagPlanData'
+
+const getCiagPlan = (id = 'G6115VJ') => stubFor(plans[id])
 
 export default {
   getCiagPlan,
