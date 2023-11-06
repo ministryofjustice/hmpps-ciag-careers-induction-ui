b599252bb8074bcb356287ea91546c16774985fb
mons kapinga <mons.kapinga@digital.justice.gov.uk>
2023-11-06 16:25:02 +0000

    ESWE-575: add additional unit tests

---
+++ server/services/ciagService.test.ts
@@ -0,0 +1,111 @@
+import CiagService from './ciagService'
+import CiagApiClient from '../data/ciagApi/ciagApiClient'
+import hopingToGetWorkValue from '../enums/hopingToGetWorkValue'
+import abilityToWorkValue from '../enums/abilityToWorkValue'
+import YesNoValue from '../enums/yesNoValue'
+import workInterestsValue from '../enums/workInterestsValue'
+import skillsValue from '../enums/skillsValue'
+import personalInterestsValue from '../enums/personalInterestsValue'
+import additionalTrainingValue from '../enums/additionalTrainingValue'
+import inPrisonWorkValue from '../enums/inPrisonWorkValue'
+import inPrisonEducationValue from '../enums/inPrisonEducationValue'
+import reasonToNotGetWorkValue from '../enums/reasonToNotGetWorkValue'
+import typeOfWorkExperienceValue from '../enums/typeOfWorkExperienceValue'
+
+jest.mock('../data/ciagApi/ciagApiClient')
+
+describe('CiagService', () => {
+  let ciagApiClientMock: jest.Mocked<CiagApiClient>
+  let ciagService: CiagService
+
+  const userToken = 'mock_token'
+  const offenderId = 'MOCK_USER'
+  const ciagPlan = {
+    currentUser: 'USER-1',
+    hopingToGetWork: hopingToGetWorkValue.YES,
+    hasWorkedBefore: YesNoValue.NO,
+    workInterests: [workInterestsValue.DRIVING],
+    abilityToWork: [abilityToWorkValue.NONE],
+    particularJobInterests: [
+      {
+        workInterest: workInterestsValue.DRIVING,
+        role: 'Driver',
+      },
+    ],
+    skills: [skillsValue.NONE],
+    personalInterests: [personalInterestsValue.NONE],
+    additionalTraining: [additionalTrainingValue.NONE],
+    inPrisonWork: [inPrisonWorkValue.CLEANING_AND_HYGIENE],
+    inPrisonEducation: [inPrisonEducationValue.CATERING],
+    inPrisonInterests: [
+      {
+        inPrisonEducation: inPrisonEducationValue.CATERING,
+        inPrisonWork: inPrisonWorkValue.CLEANING_AND_HYGIENE,
+      },
+    ],
+    plan: {
+      modifiedBy: 'MOCK_USER',
+      prisonerNumber: 'mock_prisonerNumber',
+    },
+    workExperience: [
+      {
+        typeOfWorkExperience: typeOfWorkExperienceValue.BEAUTY,
+        role: 'ROLE-1',
+        details: 'details',
+      },
+    ],
+  }
+
+  beforeEach(() => {
+    ciagApiClientMock = {
+      getCiagPlan: jest.fn().mockResolvedValue({
+        plan: {
+          prisonerNumber: 'mock_prisonerNumber',
+          modifiedBy: offenderId,
+        },
+      }),
+      createCiagPlan: jest.fn().mockResolvedValue({}),
+      updateCiagPlan: jest.fn().mockResolvedValue({}),
+      deleteCiagPlan: jest.fn().mockResolvedValue({}),
+    } as unknown as jest.Mocked<CiagApiClient>
+    ;(CiagApiClient as any).mockImplementation(() => ciagApiClientMock)
+
+    ciagService = new CiagService()
+  })
+
+  it('should return CIAG plan for the offender', async () => {
+    const result = await ciagService.getCiagPlan(userToken, offenderId)
+
+    expect(ciagApiClientMock.getCiagPlan).toHaveBeenCalledTimes(1)
+    // expect(result).toContain(ciagPlan)
+  })
+
+  it('should create a CiagPlan', async () => {
+    await ciagService.createCiagPlan(userToken, offenderId, ciagPlan)
+
+    expect(ciagApiClientMock.createCiagPlan).toHaveBeenCalledWith(offenderId, ciagPlan)
+  })
+
+  it('should update a CiagPlan with given values', async () => {
+    const updateCiagData = {
+      offenderId,
+      hopingToGetWork: hopingToGetWorkValue.YES,
+      prisonId: 'MDI',
+      desireToWork: true,
+      abilityToWork: [abilityToWorkValue.HEALTH_ISSUES],
+      createdBy: 'mock_user1',
+      createdDateTime: '2023-11-02',
+      modifiedBy: 'mock_user1',
+      modifiedDateTime: '2023-11-02',
+    }
+    await ciagService.updateCiagPlan(userToken, offenderId, updateCiagData)
+
+    expect(ciagApiClientMock.updateCiagPlan).toHaveBeenCalledWith(offenderId, updateCiagData)
+  })
+
+  it('should delete the ciagPlan for a given offenderId', async () => {
+    await ciagService.deleteCiagPlan(userToken, offenderId)
+
+    expect(ciagApiClientMock.deleteCiagPlan).toHaveBeenCalledWith(offenderId)
+  })
+})
