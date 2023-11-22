d5028c6b8b7bdfcf4d28f07cf9c197e9fca0772e
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-11-22 14:38:05 +0000

    ESWE-712_unit_tests Added check your answers tests

--- server/routes/createPlan/checkYourAnswers/checkYourAnswersController.test.ts
+++ server/routes/createPlan/checkYourAnswers/checkYourAnswersController.test.ts
@@ -5,6 +5,8 @@ import expressMocks from '../../../testutils/expressMocks'
 import Controller from './checkYourAnswersController'
 import { setSessionData } from '../../../utils/session'
 import PrisonerViewModel from '../../../viewModels/prisonerViewModel'
+import addressLookup from '../../addressLookup'
+import HopingToGetWorkValue from '../../../enums/hopingToGetWorkValue'
 
 describe('CheckYourAnswersController', () => {
   const { req, res, next } = expressMocks()
@@ -29,6 +31,7 @@ describe('CheckYourAnswersController', () => {
 
   const mockService: any = {
     createCiagPlan: jest.fn(),
+    updateCiagPlan: jest.fn(),
   }
 
   const controller = new Controller(mockService)
@@ -49,11 +52,61 @@ describe('CheckYourAnswersController', () => {
       expect(next).toHaveBeenCalledTimes(1)
     })
 
+    it('On success - No record - Redirects to hopingToGetWork', async () => {
+      setSessionData(req, ['createPlan', id], undefined)
+
+      controller.get(req, res, next)
+
+      expect(res.redirect).toHaveBeenCalledWith(addressLookup.createPlan.hopingToGetWork(id))
+    })
+
     it('On success - Calls render with the correct data', async () => {
       controller.get(req, res, next)
 
       expect(res.render).toHaveBeenCalledWith('pages/createPlan/checkYourAnswers/index', { ...mockData })
       expect(next).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  describe('#post(req, res)', () => {
+    const errors = { details: 'mock_error' }
+
+    beforeEach(() => {
+      res.render.mockReset()
+      res.redirect.mockReset()
+      next.mockReset()
+      setSessionData(req, ['inPrisonWork', id, 'data'], mockData)
+      setSessionData(req, ['createPlan', id], {})
+      mockService.createCiagPlan.mockReset()
+      mockService.updateCiagPlan.mockReset()
+    })
+
+    it('On error - Calls next with error', async () => {
+      mockService.createCiagPlan.mockImplementation(() => {
+        throw new Error('mock_error')
+      })
+
+      controller.post(req, res, next)
+
+      expect(next).toHaveBeenCalledTimes(1)
+      expect(res.render).toHaveBeenCalledTimes(0)
+    })
+
+    it('On success - UPDATE - Calls api update', async () => {
+      req.context.plan = { hopingToGetWork: HopingToGetWorkValue.NOT_SURE, desireToWork: false }
+      setSessionData(req, ['isUpdateFlow', id], true)
+      mockService.updateCiagPlan.mockReturnValue({})
+      controller.post(req, res, next)
+
+      expect(mockService.updateCiagPlan).toBeCalledTimes(1)
+    })
+
+    it('On success - NEW - Calls api acreate', async () => {
+      setSessionData(req, ['isUpdateFlow', id], undefined)
+      mockService.updateCiagPlan.mockReturnValue({})
+      controller.post(req, res, next)
+
+      expect(mockService.createCiagPlan).toBeCalledTimes(1)
     })
   })
 })
