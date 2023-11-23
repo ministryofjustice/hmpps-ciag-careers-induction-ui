95246fcdcbf87fbd725ac1ece65538bf7e936c89
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2023-11-23 13:57:22 +0000

    ESWE-712_unit_tests Removed template files from coverage report

--- server/enums/workPlanTabs.ts
+++
@@ -1,9 +0,0 @@
-enum WorkProfileTabs {
-  OVERVIEW = 'overview',
-  DETAILS = 'details',
-  EXPERIENCE = 'experience',
-  TRAINING = 'training',
-  CONTACTS = 'contacts',
-}
-
-export default WorkProfileTabs
