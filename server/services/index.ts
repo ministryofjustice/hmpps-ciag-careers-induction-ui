5d40ca2cdb24061d87153aa25b9e4454b4bbe2ef
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-11-29 11:44:14 +0000

    Eswe 712 unit tests (#93)
    * ESWE-712_unit_tests Added hopingToGetWork UPDATE unit tests
    
    * ESWE-712_unit_tests Remove console log
    
    * ESWE-712_unit_tests Added check your answers tests
    
    * ESWE-712_unit_tests Added view model tests
    
    * ESWE-712_unit_tests Removed template files from coverage report
    
    * ESWE-712_unit_tests Fixed lint errors
    
    * ESWE-712_unit_tests Barrelled utils into separate files, started unit tests
    
    * ESWE-712_unit_tests Split utils up, added utils unit tests
    
    * ESWE-712_unit_tests Removed unused data and service files
    
    * ESWE-712_unit_tests Removed unused models, added tests, removed unused config
    
    * ESWE-712_unit_tests Added final unit tests

--- server/services/index.ts
+++ server/services/index.ts
@@ -3,11 +3,6 @@ import UserService from './userService'
 import PrisonerSearchService from './prisonSearchService'
 import PaginationService from './paginationServices'
 import CuriousEsweService from './curiousEsweService'
-import KeyworkerService from './keyworkerService'
-import PrisonService from './prisonService'
-import WhereaboutsService from './whereaboutsService'
-import AllocationManagerService from './allocationManagerService'
-import CommunityService from './communityService'
 import CiagService from './ciagService'
 import EducationAndWorkPlanService from './educationAndWorkPlanService'
 import ComponentService from './componentService'
@@ -19,11 +14,6 @@ export const services = () => {
   const prisonerSearchService = new PrisonerSearchService(hmppsAuthClient)
   const paginationService = new PaginationService()
   const curiousEsweService = new CuriousEsweService(hmppsAuthClient)
-  const keyworkerService = new KeyworkerService(hmppsAuthClient)
-  const prisonService = new PrisonService(hmppsAuthClient)
-  const whereaboutsService = new WhereaboutsService(hmppsAuthClient)
-  const allocationManagerService = new AllocationManagerService(hmppsAuthClient)
-  const communityService = new CommunityService(hmppsAuthClient)
   const ciagService = new CiagService()
   const educationAndWorkPlanService = new EducationAndWorkPlanService()
   const componentService = new ComponentService()
@@ -33,11 +23,6 @@ export const services = () => {
     prisonerSearchService,
     paginationService,
     curiousEsweService,
-    keyworkerService,
-    prisonService,
-    whereaboutsService,
-    allocationManagerService,
-    communityService,
     ciagService,
     educationAndWorkPlanService,
     componentService,
