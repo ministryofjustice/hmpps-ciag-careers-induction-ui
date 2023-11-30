0d054de1743469fa3e0fb861b630e99b72a72a1e
Gary Dutton <113705362+gary-dutton-moj@users.noreply.github.com>
2023-11-30 16:07:36 +0000

    Eswe 753 feature flagged homepage change (#97)
    * ESWE-753_feature_flagged_homepage_change Added plp homepage redirect feature
    
    * ESWE-753_feature_flagged_homepage_change Tidy up and change config name to be more universal

--- server/routes/workPlan/index.ts
+++ server/routes/workPlan/index.ts
@@ -4,13 +4,18 @@ import { Services } from '../../services'
 import WorkPlanController from './workPlanController'
 import getPrisonerByIdResolver from '../../middleware/resolvers/getPrisonerByIdResolver'
 import getCiagPlanByIdResolver from '../../middleware/resolvers/getCiagPlanByIdResolver'
+import plpFrontendRedirect, { AddressLookupKey } from '../../middleware/plpFrontendRedirect'
 
 export default (router: Router, services: Services) => {
   const controller = new WorkPlanController(services.ciagService)
 
   router.get(
     '/plan/:id/view/:tab',
-    [getPrisonerByIdResolver(services.prisonerSearchService), getCiagPlanByIdResolver(services.ciagService)],
+    [
+      plpFrontendRedirect(AddressLookupKey.workPlan),
+      getPrisonerByIdResolver(services.prisonerSearchService),
+      getCiagPlanByIdResolver(services.ciagService),
+    ],
     controller.get,
   )
 }
