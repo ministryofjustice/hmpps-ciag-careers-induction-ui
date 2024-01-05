c4bb5c336507ea70e68ad089d75699dbea668c58
Adam Chrimes <adam.chrimes@digital.justice.gov.uk>
2024-01-05 11:22:48 +0000

    RR-551 add missing configuration for cypress videos

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs'
 import { defineConfig } from 'cypress'
 import { resetStubs } from './integration_tests/mockApis/wiremock'
 import auth from './integration_tests/mockApis/auth'
@@ -38,6 +39,16 @@ export default defineConfig({
         ...educationAndWorkPlanApi,
         ...frontendComponentApi,
         ...plpUi,
+      })
+      on('after:spec', (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some(test => test.attempts.some(attempt => attempt.state === 'failed'))
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
       })
     },
     baseUrl: 'http://localhost:3007',
