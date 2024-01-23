1f6b387f56ca2185b5f7a17b4925fd6ceb0e9bc9
mattwills-moj-digital <135589132+mattwills-moj-digital@users.noreply.github.com>
2024-01-23 13:24:14 +0000

    RR-596 - Create Induction client method (#121)

--- server/data/educationAndWorkPlanClient.ts
+++ server/data/educationAndWorkPlanClient.ts
@@ -1,10 +1,17 @@
-import type { InductionResponse } from 'educationAndWorkPlanApiClient'
+import type { CreateInductionRequest, InductionResponse } from 'educationAndWorkPlanApiClient'
 import RestClient from './restClient'
 import config from '../config'
 
 export default class EducationAndWorkPlanClient {
   private static restClient(token: string): RestClient {
     return new RestClient('Education and Work Plan API Client', config.apis.educationAndWorkPlanApi, token)
+  }
+
+  async createInduction(prisonNumber: string, createRequest: CreateInductionRequest, token: string): Promise<unknown> {
+    return EducationAndWorkPlanClient.restClient(token).post({
+      path: `/inductions/${prisonNumber}`,
+      data: createRequest,
+    })
   }
 
   async getInduction(prisonNumber: string, token: string): Promise<InductionResponse> {
