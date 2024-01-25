37de1c632fadc369472e6d4cbe6f7cb1c1570413
Matt Wills <matt.wills@digital.justice.gov.uk>
2024-01-25 10:34:16 +0000

    RR-597 - Use existing enums instead of strings

--- server/@types/dto/index.d.ts
+++ server/@types/dto/index.d.ts
@@ -1,3 +1,16 @@
+import HopingToGetWorkValue from '../../enums/hopingToGetWorkValue'
+import ReasonToNotGetWorkValue from '../../enums/reasonToNotGetWorkValue'
+import AbilityToWorkValue from '../../enums/abilityToWorkValue'
+import AdditionalTrainingValue from '../../enums/additionalTrainingValue'
+import EducationLevelValue from '../../enums/educationLevelValue'
+import QualificationLevelValue from '../../enums/qualificationLevelValue'
+import TypeOfWorkExperienceValue from '../../enums/typeOfWorkExperienceValue'
+import InPrisonWorkValue from '../../enums/inPrisonWorkValue'
+import InPrisonEducationValue from '../../enums/inPrisonEducationValue'
+import SkillsValue from '../../enums/skillsValue'
+import PersonalInterestsValue from '../../enums/personalInterestsValue'
+import WorkInterestsValue from '../../enums/workInterestsValue'
+
 declare module 'dto' {
   /**
    * Interface defining common reference and audit related properties that DTO types can inherit through extension.
@@ -26,56 +39,20 @@ declare module 'dto' {
   }
 
   export interface WorkOnReleaseDto extends ReferencedAndAuditable {
-    hopingToWork: 'YES' | 'NO' | 'NOT_SURE'
-    notHopingToWorkReasons: (
-      | 'LIMIT_THEIR_ABILITY'
-      | 'FULL_TIME_CARER'
-      | 'LACKS_CONFIDENCE_OR_MOTIVATION'
-      | 'HEALTH'
-      | 'RETIRED'
-      | 'NO_RIGHT_TO_WORK'
-      | 'NOT_SURE'
-      | 'OTHER'
-      | 'NO_REASON'
-    )[]
-    affectAbilityToWork: (
-      | 'CARING_RESPONSIBILITIES'
-      | 'LIMITED_BY_OFFENSE'
-      | 'HEALTH_ISSUES'
-      | 'NO_RIGHT_TO_WORK'
-      | 'OTHER'
-      | 'NONE'
-    )[]
+    hopingToWork: HopingToGetWorkValue
+    notHopingToWorkReasons: ReasonToNotGetWorkValue[]
+    affectAbilityToWork: AbilityToWorkValue[]
     notHopingToWorkOtherReason?: string
     affectAbilityToWorkOther?: string
   }
 
   export interface PreviousQualificationsDto extends ReferencedAndAuditable {
-    educationLevel:
-      | 'PRIMARY_SCHOOL'
-      | 'SECONDARY_SCHOOL_LEFT_BEFORE_TAKING_EXAMS'
-      | 'SECONDARY_SCHOOL_TOOK_EXAMS'
-      | 'FURTHER_EDUCATION_COLLEGE'
-      | 'UNDERGRADUATE_DEGREE_AT_UNIVERSITY'
-      | 'POSTGRADUATE_DEGREE_AT_UNIVERSITY'
-      | 'NOT_SURE'
+    educationLevel: EducationLevelValue
     qualifications: Array<AchievedQualificationDto>
   }
 
   export interface PreviousTrainingDto extends ReferencedAndAuditable {
-    trainingTypes: (
-      | 'CSCS_CARD'
-      | 'FIRST_AID_CERTIFICATE'
-      | 'FOOD_HYGIENE_CERTIFICATE'
-      | 'FULL_UK_DRIVING_LICENCE'
-      | 'HEALTH_AND_SAFETY'
-      | 'HGV_LICENCE'
-      | 'MACHINERY_TICKETS'
-      | 'MANUAL_HANDLING'
-      | 'TRADE_COURSE'
-      | 'OTHER'
-      | 'NONE'
-    )[]
+    trainingTypes: AdditionalTrainingValue[]
     trainingTypeOther?: string
   }
 
@@ -110,56 +87,20 @@ declare module 'dto' {
   }
 
   export interface CreateWorkOnReleaseDto {
-    hopingToWork: 'YES' | 'NO' | 'NOT_SURE'
-    notHopingToWorkReasons?: (
-      | 'LIMIT_THEIR_ABILITY'
-      | 'FULL_TIME_CARER'
-      | 'LACKS_CONFIDENCE_OR_MOTIVATION'
-      | 'HEALTH'
-      | 'RETIRED'
-      | 'NO_RIGHT_TO_WORK'
-      | 'NOT_SURE'
-      | 'OTHER'
-      | 'NO_REASON'
-    )[]
+    hopingToWork: HopingToGetWorkValue
+    notHopingToWorkReasons?: ReasonToNotGetWorkValue[]
     notHopingToWorkOtherReason?: string
-    affectAbilityToWork?: (
-      | 'CARING_RESPONSIBILITIES'
-      | 'LIMITED_BY_OFFENSE'
-      | 'HEALTH_ISSUES'
-      | 'NO_RIGHT_TO_WORK'
-      | 'OTHER'
-      | 'NONE'
-    )[]
+    affectAbilityToWork?: AbilityToWorkValue[]
     affectAbilityToWorkOther?: string
   }
 
   export interface CreatePreviousQualificationsDto {
-    educationLevel?:
-      | 'PRIMARY_SCHOOL'
-      | 'SECONDARY_SCHOOL_LEFT_BEFORE_TAKING_EXAMS'
-      | 'SECONDARY_SCHOOL_TOOK_EXAMS'
-      | 'FURTHER_EDUCATION_COLLEGE'
-      | 'UNDERGRADUATE_DEGREE_AT_UNIVERSITY'
-      | 'POSTGRADUATE_DEGREE_AT_UNIVERSITY'
-      | 'NOT_SURE'
+    educationLevel?: EducationLevelValue
     qualifications?: AchievedQualificationDto[]
   }
 
   export interface CreatePreviousTrainingDto {
-    trainingTypes: (
-      | 'CSCS_CARD'
-      | 'FIRST_AID_CERTIFICATE'
-      | 'FOOD_HYGIENE_CERTIFICATE'
-      | 'FULL_UK_DRIVING_LICENCE'
-      | 'HEALTH_AND_SAFETY'
-      | 'HGV_LICENCE'
-      | 'MACHINERY_TICKETS'
-      | 'MANUAL_HANDLING'
-      | 'TRADE_COURSE'
-      | 'OTHER'
-      | 'NONE'
-    )[]
+    trainingTypes: AdditionalTrainingValue[]
     trainingTypeOther?: string
   }
 
@@ -184,117 +125,39 @@ declare module 'dto' {
 
   export interface AchievedQualificationDto {
     subject: string
-    level: 'ENTRY_LEVEL' | 'LEVEL_1' | 'LEVEL_2' | 'LEVEL_3' | 'LEVEL_4' | 'LEVEL_5' | 'LEVEL_6' | 'LEVEL_7' | 'LEVEL_8'
+    level: QualificationLevelValue
     grade: string
   }
 
   export interface PreviousWorkExperienceDto {
-    experienceType:
-      | 'OUTDOOR'
-      | 'CONSTRUCTION'
-      | 'DRIVING'
-      | 'BEAUTY'
-      | 'HOSPITALITY'
-      | 'TECHNICAL'
-      | 'MANUFACTURING'
-      | 'OFFICE'
-      | 'RETAIL'
-      | 'SPORTS'
-      | 'WAREHOUSING'
-      | 'WASTE_MANAGEMENT'
-      | 'EDUCATION_TRAINING'
-      | 'CLEANING_AND_MAINTENANCE'
-      | 'OTHER'
+    experienceType: TypeOfWorkExperienceValue
     experienceTypeOther?: string
     role?: string
     details?: string
   }
 
   export interface InPrisonWorkInterestDto {
-    workType:
-      | 'CLEANING_AND_HYGIENE'
-      | 'COMPUTERS_OR_DESK_BASED'
-      | 'GARDENING_AND_OUTDOORS'
-      | 'KITCHENS_AND_COOKING'
-      | 'MAINTENANCE'
-      | 'PRISON_LAUNDRY'
-      | 'PRISON_LIBRARY'
-      | 'TEXTILES_AND_SEWING'
-      | 'WELDING_AND_METALWORK'
-      | 'WOODWORK_AND_JOINERY'
-      | 'OTHER'
+    workType: InPrisonWorkValue
     workTypeOther?: string
   }
 
   export interface InPrisonTrainingInterestDto {
-    trainingType:
-      | 'BARBERING_AND_HAIRDRESSING'
-      | 'CATERING'
-      | 'COMMUNICATION_SKILLS'
-      | 'ENGLISH_LANGUAGE_SKILLS'
-      | 'FORKLIFT_DRIVING'
-      | 'INTERVIEW_SKILLS'
-      | 'MACHINERY_TICKETS'
-      | 'NUMERACY_SKILLS'
-      | 'RUNNING_A_BUSINESS'
-      | 'SOCIAL_AND_LIFE_SKILLS'
-      | 'WELDING_AND_METALWORK'
-      | 'WOODWORK_AND_JOINERY'
-      | 'OTHER'
+    trainingType: InPrisonEducationValue
     trainingTypeOther?: string
   }
 
   export interface PersonalSkillDto {
-    skillType:
-      | 'COMMUNICATION'
-      | 'POSITIVE_ATTITUDE'
-      | 'RESILIENCE'
-      | 'SELF_MANAGEMENT'
-      | 'TEAMWORK'
-      | 'THINKING_AND_PROBLEM_SOLVING'
-      | 'WILLINGNESS_TO_LEARN'
-      | 'OTHER'
-      | 'NONE'
+    skillType: SkillsValue
     skillTypeOther?: string
   }
 
   export interface PersonalInterestDto {
-    interestType:
-      | 'COMMUNITY'
-      | 'CRAFTS'
-      | 'CREATIVE'
-      | 'DIGITAL'
-      | 'KNOWLEDGE_BASED'
-      | 'MUSICAL'
-      | 'OUTDOOR'
-      | 'NATURE_AND_ANIMALS'
-      | 'SOCIAL'
-      | 'SOLO_ACTIVITIES'
-      | 'SOLO_SPORTS'
-      | 'TEAM_SPORTS'
-      | 'WELLNESS'
-      | 'OTHER'
-      | 'NONE'
+    interestType: PersonalInterestsValue
     interestTypeOther?: string
   }
 
   export interface FutureWorkInterestDto {
-    workType:
-      | 'OUTDOOR'
-      | 'CONSTRUCTION'
-      | 'DRIVING'
-      | 'BEAUTY'
-      | 'HOSPITALITY'
-      | 'TECHNICAL'
-      | 'MANUFACTURING'
-      | 'OFFICE'
-      | 'RETAIL'
-      | 'SPORTS'
-      | 'WAREHOUSING'
-      | 'WASTE_MANAGEMENT'
-      | 'EDUCATION_TRAINING'
-      | 'CLEANING_AND_MAINTENANCE'
-      | 'OTHER'
+    workType: WorkInterestsValue
     workTypeOther?: string
     role?: string
   }
--- server/testsupport/createInductionDtoTestDataBuilder.ts
+++ server/testsupport/createInductionDtoTestDataBuilder.ts
@@ -1,4 +1,16 @@
 import type { CreateInductionDto } from 'dto'
+import HopingToGetWorkValue from '../enums/hopingToGetWorkValue'
+import AbilityToWorkValue from '../enums/abilityToWorkValue'
+import TypeOfWorkExperienceValue from '../enums/typeOfWorkExperienceValue'
+import WorkInterestsValue from '../enums/workInterestsValue'
+import SkillsValue from '../enums/skillsValue'
+import PersonalInterestsValue from '../enums/personalInterestsValue'
+import EducationLevelValue from '../enums/educationLevelValue'
+import QualificationLevelValue from '../enums/qualificationLevelValue'
+import AdditionalTrainingValue from '../enums/additionalTrainingValue'
+import ReasonToNotGetWorkValue from '../enums/reasonToNotGetWorkValue'
+import InPrisonWorkValue from '../enums/inPrisonWorkValue'
+import InPrisonEducationValue from '../enums/inPrisonEducationValue'
 
 const aCreateLongQuestionSetInductionDto = (
   hasWorkedBefore?: boolean,
@@ -8,8 +20,8 @@ const aCreateLongQuestionSetInductionDto = (
   return {
     ...baseCreateInductionDtoTemplate(),
     workOnRelease: {
-      hopingToWork: 'YES',
-      affectAbilityToWork: ['NONE'],
+      hopingToWork: HopingToGetWorkValue.YES,
+      affectAbilityToWork: [AbilityToWorkValue.NONE],
       affectAbilityToWorkOther: null,
       notHopingToWorkReasons: null,
       notHopingToWorkOtherReason: null,
@@ -20,13 +32,13 @@ const aCreateLongQuestionSetInductionDto = (
         hasWorkedBefore === null || hasWorkedBefore === undefined || hasWorkedBefore === true
           ? [
               {
-                experienceType: 'CONSTRUCTION',
+                experienceType: TypeOfWorkExperienceValue.CONSTRUCTION,
                 experienceTypeOther: null,
                 role: 'General labourer',
                 details: 'Groundwork and basic block work and bricklaying',
               },
               {
-                experienceType: 'OTHER',
+                experienceType: TypeOfWorkExperienceValue.OTHER,
                 experienceTypeOther: 'Retail delivery',
                 role: 'Milkman',
                 details: 'Self employed franchise operator delivering milk and associated diary products.',
@@ -37,17 +49,17 @@ const aCreateLongQuestionSetInductionDto = (
     futureWorkInterests: {
       interests: [
         {
-          workType: 'RETAIL',
+          workType: WorkInterestsValue.RETAIL,
           workTypeOther: null,
           role: null,
         },
         {
-          workType: 'CONSTRUCTION',
+          workType: WorkInterestsValue.CONSTRUCTION,
           workTypeOther: null,
           role: 'General labourer',
         },
         {
-          workType: 'OTHER',
+          workType: WorkInterestsValue.OTHER,
           workTypeOther: 'Film, TV and media',
           role: 'Being a stunt double for Tom Cruise, even though he does all his own stunts',
         },
@@ -57,56 +69,62 @@ const aCreateLongQuestionSetInductionDto = (
       skills:
         hasSkills === null || hasSkills === undefined || hasSkills === true
           ? [
-              { skillType: 'TEAMWORK', skillTypeOther: null },
-              { skillType: 'WILLINGNESS_TO_LEARN', skillTypeOther: null },
-              { skillType: 'OTHER', skillTypeOther: 'Tenacity' },
+              { skillType: SkillsValue.TEAMWORK, skillTypeOther: null },
+              { skillType: SkillsValue.WILLINGNESS_TO_LEARN, skillTypeOther: null },
+              { skillType: SkillsValue.OTHER, skillTypeOther: 'Tenacity' },
             ]
           : [],
       interests:
         hasInterests === null || hasInterests === undefined || hasInterests === true
           ? [
-              { interestType: 'CREATIVE', interestTypeOther: null },
-              { interestType: 'DIGITAL', interestTypeOther: null },
-              { interestType: 'OTHER', interestTypeOther: 'Renewable energy' },
+              { interestType: PersonalInterestsValue.CREATIVE, interestTypeOther: null },
+              { interestType: PersonalInterestsValue.DIGITAL, interestTypeOther: null },
+              { interestType: PersonalInterestsValue.OTHER, interestTypeOther: 'Renewable energy' },
             ]
           : [],
     },
     previousQualifications: {
-      educationLevel: 'SECONDARY_SCHOOL_TOOK_EXAMS',
+      educationLevel: EducationLevelValue.SECONDARY_SCHOOL_TOOK_EXAMS,
       qualifications: [
         {
           subject: 'Pottery',
           grade: 'C',
-          level: 'LEVEL_4',
+          level: QualificationLevelValue.LEVEL_4,
         },
       ],
     },
     previousTraining: {
-      trainingTypes: ['FIRST_AID_CERTIFICATE', 'MANUAL_HANDLING', 'OTHER'],
+      trainingTypes: [
+        AdditionalTrainingValue.FIRST_AID_CERTIFICATE,
+        AdditionalTrainingValue.MANUAL_HANDLING,
+        AdditionalTrainingValue.OTHER,
+      ],
       trainingTypeOther: 'Advanced origami',
     },
   }
 }
 
-const aCreateShortQuestionSetInductionDto = (hopingToGetWork?: 'NO' | 'NOT_SURE'): CreateInductionDto => {
+const aCreateShortQuestionSetInductionDto = (
+  hopingToGetWork?: HopingToGetWorkValue.NO | HopingToGetWorkValue.NOT_SURE,
+): CreateInductionDto => {
   return {
     ...baseCreateInductionDtoTemplate(),
     workOnRelease: {
-      hopingToWork: hopingToGetWork || 'NO',
+      hopingToWork: hopingToGetWork || HopingToGetWorkValue.NO,
       affectAbilityToWork: null,
       affectAbilityToWorkOther: null,
-      notHopingToWorkReasons: ['HEALTH', 'OTHER'],
+      notHopingToWorkReasons: [ReasonToNotGetWorkValue.HEALTH, ReasonToNotGetWorkValue.OTHER],
       notHopingToWorkOtherReason: 'Will be of retirement age at release',
     },
     inPrisonInterests: {
       inPrisonWorkInterests: [
-        { workType: 'CLEANING_AND_HYGIENE', workTypeOther: null },
-        { workType: 'OTHER', workTypeOther: 'Gardening and grounds keeping' },
+        { workType: InPrisonWorkValue.CLEANING_AND_HYGIENE, workTypeOther: null },
+        { workType: InPrisonWorkValue.OTHER, workTypeOther: 'Gardening and grounds keeping' },
       ],
       inPrisonTrainingInterests: [
-        { trainingType: 'FORKLIFT_DRIVING', trainingTypeOther: null },
-        { trainingType: 'CATERING', trainingTypeOther: null },
-        { trainingType: 'OTHER', trainingTypeOther: 'Advanced origami' },
+        { trainingType: InPrisonEducationValue.FORKLIFT_DRIVING, trainingTypeOther: null },
+        { trainingType: InPrisonEducationValue.CATERING, trainingTypeOther: null },
+        { trainingType: InPrisonEducationValue.OTHER, trainingTypeOther: 'Advanced origami' },
       ],
     },
     previousQualifications: {
@@ -115,17 +133,17 @@ const aCreateShortQuestionSetInductionDto = (hopingToGetWork?: 'NO' | 'NOT_SURE'
         {
           subject: 'English',
           grade: 'C',
-          level: 'LEVEL_6',
+          level: QualificationLevelValue.LEVEL_6,
         },
         {
           subject: 'Maths',
           grade: 'A*',
-          level: 'LEVEL_6',
+          level: QualificationLevelValue.LEVEL_6,
         },
       ],
     },
     previousTraining: {
-      trainingTypes: ['FULL_UK_DRIVING_LICENCE', 'OTHER'],
+      trainingTypes: [AdditionalTrainingValue.FULL_UK_DRIVING_LICENCE, AdditionalTrainingValue.OTHER],
       trainingTypeOther: 'Beginners cookery for IT professionals',
     },
   }
