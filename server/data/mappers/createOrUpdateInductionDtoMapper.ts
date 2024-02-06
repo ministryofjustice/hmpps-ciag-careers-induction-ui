36d0d90dbde7838ce57a31acea50c38004b94577
mattwills-moj-digital <135589132+mattwills-moj-digital@users.noreply.github.com>
2024-02-06 15:23:08 +0000

    RR-597 - createOrUpdateInductionDtoMapper (#127)

---
+++ server/data/mappers/createOrUpdateInductionDtoMapper.ts
@@ -0,0 +1,121 @@
+import type { CreateOrUpdateInductionDto } from 'dto'
+import CiagPlan from '../ciagApi/interfaces/ciagPlan'
+
+const toCreateOrUpdateInductionDto = (ciagPlan: CiagPlan): CreateOrUpdateInductionDto => {
+  return {
+    reference: undefined,
+    prisonId: ciagPlan.prisonId,
+    workOnRelease: toCreateOrUpdateWorkOnReleaseDto(ciagPlan),
+    previousQualifications: toCreateOrUpdatePreviousQualificationsDto(ciagPlan),
+    previousTraining: toCreateOrUpdatePreviousTrainingDto(ciagPlan),
+    previousWorkExperiences: toCreateOrUpdatePreviousWorkExperiencesDto(ciagPlan),
+    inPrisonInterests: toCreateOrUpdateInPrisonInterestsDto(ciagPlan),
+    personalSkillsAndInterests: toCreateOrUpdatePersonalSkillsAndInterestsDto(ciagPlan),
+    futureWorkInterests: toCreateOrUpdateFutureWorkInterestsDto(ciagPlan),
+  }
+}
+
+const toCreateOrUpdateWorkOnReleaseDto = (ciagPlan: CiagPlan) => {
+  return {
+    hopingToWork: ciagPlan.hopingToGetWork,
+    notHopingToWorkReasons: ciagPlan.reasonToNotGetWork,
+    notHopingToWorkOtherReason: ciagPlan.reasonToNotGetWorkOther,
+    affectAbilityToWork: ciagPlan.abilityToWork,
+    affectAbilityToWorkOther: ciagPlan.abilityToWorkOther,
+  }
+}
+const toCreateOrUpdatePreviousQualificationsDto = (ciagPlan: CiagPlan) => {
+  return ciagPlan.qualificationsAndTraining
+    ? {
+        educationLevel: ciagPlan.qualificationsAndTraining.educationLevel,
+        qualifications: ciagPlan.qualificationsAndTraining.qualifications.map(qualification => {
+          return {
+            subject: qualification.subject,
+            grade: qualification.grade,
+            level: qualification.level,
+          }
+        }),
+      }
+    : undefined
+}
+const toCreateOrUpdatePreviousTrainingDto = (ciagPlan: CiagPlan) => {
+  return ciagPlan.qualificationsAndTraining
+    ? {
+        trainingTypes: ciagPlan.qualificationsAndTraining.additionalTraining,
+        trainingTypeOther: ciagPlan.qualificationsAndTraining.additionalTrainingOther,
+      }
+    : undefined
+}
+
+const toCreateOrUpdatePreviousWorkExperiencesDto = (ciagPlan: CiagPlan) => {
+  return ciagPlan.workExperience
+    ? {
+        hasWorkedBefore: ciagPlan.workExperience.hasWorkedBefore,
+        experiences: ciagPlan.workExperience.workExperience.map(experience => {
+          return {
+            experienceType: experience.typeOfWorkExperience,
+            experienceTypeOther:
+              experience.typeOfWorkExperience === 'OTHER'
+                ? ciagPlan.workExperience.typeOfWorkExperienceOther
+                : undefined,
+            role: experience.role,
+            details: experience.details,
+          }
+        }),
+      }
+    : undefined
+}
+const toCreateOrUpdateInPrisonInterestsDto = (ciagPlan: CiagPlan) => {
+  return ciagPlan.inPrisonInterests
+    ? {
+        inPrisonWorkInterests: ciagPlan.inPrisonInterests.inPrisonWork.map(workInterest => {
+          return {
+            workType: workInterest,
+            workTypeOther: workInterest === 'OTHER' ? ciagPlan.inPrisonInterests.inPrisonWorkOther : undefined,
+          }
+        }),
+        inPrisonTrainingInterests: ciagPlan.inPrisonInterests.inPrisonEducation.map(trainingInterest => {
+          return {
+            trainingType: trainingInterest,
+            trainingTypeOther:
+              trainingInterest === 'OTHER' ? ciagPlan.inPrisonInterests.inPrisonEducationOther : undefined,
+          }
+        }),
+      }
+    : undefined
+}
+const toCreateOrUpdatePersonalSkillsAndInterestsDto = (ciagPlan: CiagPlan) => {
+  return ciagPlan.skillsAndInterests
+    ? {
+        skills: ciagPlan.skillsAndInterests.skills.map(skill => {
+          return {
+            skillType: skill,
+            skillTypeOther: skill === 'OTHER' ? ciagPlan.skillsAndInterests.skillsOther : undefined,
+          }
+        }),
+        interests: ciagPlan.skillsAndInterests.personalInterests.map(interest => {
+          return {
+            interestType: interest,
+            interestTypeOther: interest === 'OTHER' ? ciagPlan.skillsAndInterests.personalInterestsOther : undefined,
+          }
+        }),
+      }
+    : undefined
+}
+const toCreateOrUpdateFutureWorkInterestsDto = (ciagPlan: CiagPlan) => {
+  return ciagPlan.workExperience?.workInterests
+    ? {
+        interests: ciagPlan.workExperience.workInterests.workInterests.map(interest => {
+          return {
+            workType: interest,
+            workTypeOther: interest === 'OTHER' ? ciagPlan.workExperience.workInterests.workInterestsOther : undefined,
+            role: ciagPlan.workExperience.workInterests.particularJobInterests?.find(
+              element => element.workInterest === interest,
+            )?.role,
+          }
+        }),
+      }
+    : undefined
+}
+
+export default toCreateOrUpdateInductionDto
