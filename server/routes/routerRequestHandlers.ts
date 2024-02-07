7119e52397bedf40fc6f7397dbd78b79c6b8b865
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2024-02-07 11:23:23 +0000

    RR-570 - Use InductionService to retrieve Inductions rather than CiagService, based on feature toggle (#128)

--- server/routes/routerRequestHandlers.ts
+++ server/routes/routerRequestHandlers.ts
@@ -1,19 +1,26 @@
 import { NextFunction, Request, Response, RequestHandler } from 'express'
 import createError from 'http-errors'
-import { CiagService } from '../services'
+import { CiagService, InductionService } from '../services'
 import logger from '../../logger'
+import config from '../config'
+import toCiagPlan from '../data/mappers/ciagPlanMapper'
 
 /**
- *  Middleware function that returns a Request handler function to retrieve the Induction via the [CiagService] and
- *  store in the express request context.
+ *  Middleware function that returns a Request handler function to retrieve the [InductionDto] via the [InductionService]
+ *  and store in the express request context as a [CiagPlan].
  */
-const retrieveInduction = (ciagService: CiagService): RequestHandler => {
+const retrieveInduction = (ciagService: CiagService, inductionService: InductionService): RequestHandler => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const { id: prisonNumber } = req.params
     const { user } = res.locals
 
     try {
-      req.context.plan = await ciagService.getCiagPlan(user.token, prisonNumber)
+      if (config.featureToggles.useNewInductionApiEnabled) {
+        const inductionDto = await inductionService.getInduction(prisonNumber, user.token)
+        req.context.plan = toCiagPlan(inductionDto)
+      } else {
+        req.context.plan = await ciagService.getCiagPlan(user.token, prisonNumber)
+      }
       next()
     } catch (error) {
       logger.error(`Error retrieving Induction for Prisoner ${prisonNumber}`, error)
