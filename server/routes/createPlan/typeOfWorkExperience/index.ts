62607d8dbab77468fea5dacbe47089ec2d7eb38e
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2024-02-14 09:30:35 +0000

    RR-631 - Use new Inductions API (Type of Work Experience to Work Interests) (#141)

--- server/routes/createPlan/typeOfWorkExperience/index.ts
+++ server/routes/createPlan/typeOfWorkExperience/index.ts
@@ -7,7 +7,7 @@ import TypeOfWorkExperienceController from './typeOfWorkExperienceController'
 import checkSessionPageData from '../../../middleware/checkSessionPageData'
 
 export default (router: Router, services: Services) => {
-  const controller = new TypeOfWorkExperienceController(services.ciagService)
+  const controller = new TypeOfWorkExperienceController(services.ciagService, services.inductionService)
 
   router.get(
     '/plan/create/:id/type-of-work-experience/:mode',
