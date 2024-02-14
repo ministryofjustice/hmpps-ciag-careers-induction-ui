354e26e644bc66d607e6064075de7bcd04145a95
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2024-02-14 10:14:46 +0000

    RR-631 - Use new Inductions API (Qualifications to Skills) (#143)

--- server/routes/createPlan/reasonToNotGetWork/index.ts
+++ server/routes/createPlan/reasonToNotGetWork/index.ts
@@ -7,7 +7,7 @@ import ReasonToNotGetWorkController from './reasonToNotGetWorkController'
 import checkSessionPageData from '../../../middleware/checkSessionPageData'
 
 export default (router: Router, services: Services) => {
-  const controller = new ReasonToNotGetWorkController(services.ciagService)
+  const controller = new ReasonToNotGetWorkController(services.ciagService, services.inductionService)
 
   router.get(
     '/plan/create/:id/reason-to-not-get-work/:mode',
